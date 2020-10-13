0ed366f0e7f285bd325dca2c389fae1ecaf0684e
astitvajain <72643435+astitvajain@users.noreply.github.com>
2020-10-13 14:41:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# class-20-collision-algorithm
-visual studio
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(800,400);
+  fr = createSprite(200, 200, 50, 80);
+  fr.shapeColor = "green";
+  mr = createSprite(400,200,80,30);
+  mr.shapeColor = "green";
+}
+
+function draw() {
+  background(255,255,255);
+  mr.x=World.mouseX;
+  mr.y=World.mouseY;
+  if (mr.x-fr.x<mr.width/2+fr.width/2
+    && fr.x-mr.x<fr.width/2+mr.width/2
+    && mr.y-fr.y<mr.height/2+fr.height/2
+    && fr.y-mr.y<fr.height/2+mr.height/2) {
+      mr.shapeColor = "red";
+      fr.shapeColor = "red";
+    } else {
+      mr.shapeColor = "green";
+      fr.shapeColor = "green";
+    }
+  drawSprites();
+}
